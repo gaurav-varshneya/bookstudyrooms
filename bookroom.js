fa6e53ba8e22ae0f37f8f90890a7bc353f567eb7
gauravvarshneya <gvarsh.21@gmail.com>
2018-05-09 17:24:45 -0700

    iteration complete

--- bookroom.js
+++ bookroom.js
@@ -3,53 +3,70 @@ const argv = require('yargs').argv;
 
 const LOGIN = async (page) => {
     const USER = {
-        username : "",
-        password : "",
-        weekOfTheMonth : argv.mt || "2",
-        day : argv.d || "1",
+        username : "USERNAME GOES HERE",
+        password : "PASSWORD GOES HERE",
 
-        date : "02/28/2018"
+        date : argv.d || "05/16/2018",
+        name : argv.n || "automated booking TEST"
     }
 
     try {
 
         const emailField = await page.$('input[name=j_username]');
-        await emailField.click({delay: 10});
-        await emailField.type(USER.username, {delay: 10});
+        await emailField.click();
+        await emailField.type(USER.username);
         await emailField.dispose();
 
         const passwordField = await page.$('input[name=j_password]');
-        await passwordField.click({delay: 10});
-        await passwordField.type(USER.password, {delay: 10});
+        await passwordField.click();
+        await passwordField.type(USER.password);
         await passwordField.dispose();
 
         const loginButton = await page.$('input[type=submit]');
         await loginButton.focus();
-        await loginButton.click({ delay: 10 });
+        await loginButton.click();
         await loginButton.dispose();
-
-        await page.click('#datepicker');
-    
-
-        /*const dateSelect = await page.$('input[id=datepicker]');
-        await dateSelect.focus();
-        await dateSelect.click({delay: 100});
-        await dateSelect.type(USER.date, {delay: 100});*/
         
-        /*await page.click('#day_main > tbody > tr:nth-child(31) > td:nth-child(15) > div > a');
-        await page.waitForNavigation();*/
 
-        //const theDaySelector = '#cal_this > table > tbody > tr:nth-child(5) > td:nth-child(4) > a';
-        //await page.waitForSelector(theDaySelector);
+        const dateSelect = await page.waitForSelector('input[id=datepicker]');
+        await dateSelect.focus();
+        for (let i = 0; i < 10; i++){
+            await page.keyboard.press('Backspace');
+        }
+        await dateSelect.type(USER.date, {delay: 2});
+
+        const goToButton = await page.$('input[type=submit]');
+        await goToButton.focus();
+        await goToButton.click();
+        await goToButton.dispose();
+       
+        const booking = await page.waitForSelector('#day_main > tbody > tr:nth-child(1) > td:nth-child(2) > div > a');
+        await booking.focus();
+        await booking.click();
+        await booking.dispose();
+
+        const theRoom = await page.waitForSelector('input[type=text]');
+        await theRoom.click();
+        await theRoom.type(USER.name);
+
+        const saveBooking = await page.$('input[name=save_button]');
+        await saveBooking.focus();
+        await saveBooking.click();
+        await saveBooking.dispose();
+
+        console.log('Room Booked!');
+
+        process.exit();
+        
 
     }
     catch(err){
-        console.log('lol whoops');
+        console.log('error');
     }
 };
 
 async function run() {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
     await page.goto('https://booking.sauder.ubc.ca/ugr/cwl-login');
     await LOGIN(page);
